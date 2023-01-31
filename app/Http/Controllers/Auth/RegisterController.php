ad362f19e641abb2ae8649dd2e2deba189bd003d
amirrezaraadi <amirrezaraadi@yahoo.com>
2023-01-31 14:50:07 +0330

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Cryptommer\Smsir\Classes\Smsir;
+use Cryptommer\Smsir\Objects\Parameters;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'min:10' ],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        $users = User::create([
+            'name' => $data['name'],
+            'phone' => $data['phone'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $send = Smsir::Send();
+        $templateId ="518086";
+        $mobile = $users->phone;
+        $sms_code = rand(1111 , 9999);
+
+        $parameters = array(new Parameters("code" , $sms_code));
+        $send->Verify($mobile, $templateId, $parameters );
+
+
+        $message = $users->name .'کد یکبار مصرف ارسال شد ';
+        return  response($message);
+    }
+}
